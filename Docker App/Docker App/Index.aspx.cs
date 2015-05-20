8c5e783b4abf7b1e06c4100ec3662cb0cfae22dc
mohanji <mohan.bmdl@gmail.com>
2015-05-21 00:44:32 +0530

    poling

--- Docker App/Docker App/Index.aspx.cs
+++ Docker App/Docker App/Index.aspx.cs
@@ -11,7 +11,7 @@ namespace Docker_App
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            DocBut.Text = "Changes";
+            DocBut.Text = "Change";
         }
     }
 }
